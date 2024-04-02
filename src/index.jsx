4a3b0963a4d058437f4e55b6caedb84eec77b5d1
Natalia Rodriguez Arias <rodrig@kth.se>
2024-04-02 14:44:25 +0200

    testing pokemon API

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,13 @@
 import {createRoot} from "react-dom/client";
 createRoot(document.getElementById('root'))
     .render(<div>hello world!</div>);  
+
+    const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    
+    try {
+        const response = await fetch(url);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
