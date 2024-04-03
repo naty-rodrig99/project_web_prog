7b07f31bbdaa5f52e22f1e97b8aa7956a65be533
Filip Forsberg <filfor@kth.se>
2024-04-03 12:41:33 +0200

    bugs with yooki

--- src/views/mainView.jsx
+++ src/views/mainView.jsx
@@ -1,13 +1,13 @@
 import "../style.css"
 export function MainView(props){
-    function logACB(evt){
-        console.log('Clicked')
-        console.log(props.search("ditto"))
-        console.log(evt)
-    }
     return (
     <div class="mainContainer">
         <button onClick={logACB}>Click me!</button>
     </div>
     )
+    function logACB(evt){
+        console.log('Clicked')
+        console.log(props.search("ditto"))
+        console.log(evt)
+    }
 }
