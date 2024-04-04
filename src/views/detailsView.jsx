7516ada437bf14e7fb907ee0919c041c7666d9fb
Natalia Rodriguez Arias <rodrig@kth.se>
2024-04-04 12:10:20 +0200

    new font experiment

--- src/views/detailsView.jsx
+++ src/views/detailsView.jsx
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
 
 export function DetailsView(props){
+    const [heartImgSrc, setHeartImgSrc] = useState('src/icons/empty_heart.png');
 
     function showDetailsACB(){
         props.pokemonFunction()
@@ -9,16 +11,24 @@ export function DetailsView(props){
         props.abilitiesFunction()
     }
 
+    function handleHeartClick(){
+        const newHeartImg = 'src/icons/red_heart.png';
+        setHeartImgSrc(newHeartImg);
+    }
+
     return (
         <div>
-            <button onClick={showDetailsACB}>Show</button>
             <div class="details_part1">
                 <img class="details_part1_img1" src={props.pokemon.sprites.front_shiny}></img>
                 <div class="details_part1_name">{props.pokemon.name}</div>
-                <img class="details_part1_img2" src='src\icons\empty_heart.png'></img>
+                <button onClick={handleHeartClick} className="details_part1_button-img2">
+                    <img class="details_part1_img2" src={heartImgSrc}></img>
+                </button>
             </div>
             <div class="details_part2">
                 <button onClick={showAbilitiesACB} class="details_part2_abilities">Details</button>
+                <button class="details_part2_abilities">Sprites</button>
+                <button class="details_part2_abilities">Forum</button>
                 <div class="details_part2_line"></div>
                 <div class="details_part2_smallLine"></div>
                 <div class="details_part2_headline1">Abilities </div>
@@ -27,6 +37,9 @@ export function DetailsView(props){
             <div class="details_part2_abilities1">
                 {props.pokemon.abilities.map(abilitiesACB)}
             </div>
+
+            <div > </div>
+
             
             
         </div>
