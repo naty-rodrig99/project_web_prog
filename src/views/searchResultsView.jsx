1e9283610a14f3359713b3ddad1205dfb28f872a
ZHANG Yujing <yujingzh@kth.se>
2024-04-04 23:05:12 +0200

    connect learn more button with the details page

--- src/views/searchResultsView.jsx
+++ src/views/searchResultsView.jsx
@@ -1,12 +1,19 @@
 import "../style.css"
 export function SearchResultsView(props){
+
+    console.log("props!!!!!!!!!!!", props);
+    function changeToDetails(){
+        props.detailsChosenACB(props.searchResults.id);
+        window.location.hash="#/details";
+    }
+
     return (
         <div className="searchResultsView">
             <div className="resultCard">
                 {spriteDefaultOrShinyACB()}
                 <ul>
                     <li className="resultCardName">{props.searchResults.name}</li>
-                    <li><button>Learn More</button></li>
+                    <li><button onClick={changeToDetails}>Learn More</button></li>
                 </ul>
             </div>
         </div>
