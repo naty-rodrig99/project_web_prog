cd53db112627b1b6dc4058c0de3a644cb9503184
Natalia Rodriguez Arias <rodrig@kth.se>
2024-05-07 15:35:04 +0200

    created table in team page

--- src/views/teamView.jsx
+++ src/views/teamView.jsx
@@ -14,8 +14,27 @@ export function TeamView(props){
                 <div class = "team_view1_title">My Teams</div>
             </div>    
             <div>
-                <button className="details_part1_backbutton">Create Team</button>
+                <button className="team_createteam_button">Create Team</button>
+            </div>
+            <div class="team_table-container">
+            <table>
+                <thead class="team_table-container_th">
+                    <tr>
+                        <th>Team Name</th>
+                        <th>Pokemon Team Members</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Team 1</td>
+                        <td>ditto - weedle - pikachu - bulbasaur - raichu</td>
+                        <td><button class="team_seedetails_button">See Details</button></td>
+                    </tr>
+                </tbody>
+            </table>
             </div>
         </div>
+        
     );
 }
